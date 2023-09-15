68ccd2459069ed58fb5a46f3996cc7a6b4c01b67
Yuri Gabriel <yuri@aqua.com.br>
2023-09-15 13:56:35 -0300

    Adding Consumer code

--- POCRabbitMQ/Consumer/Program.cs
+++ POCRabbitMQ/Consumer/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+//This line is to ensure the queue is created, not to create it.
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+Console.WriteLine("Esperando por mensagens");
+
+//Here we define the behaviour after message received
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine(message);
+};
+
+//This point do the binding on the queue
+channel.BasicConsume(queue: "hello",
+    autoAck: true,
+    consumer: consumer);
+
+Console.ReadLine();
